83af2a04cc5c3b09f316feb39256e6ccd5b318f7
Gustavo Harff <gustavo.harff@gmail.com>
2020-08-31 13:02:41 -0300

    feat: dashboard style

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
-    <Container>
-      <h1>Explore repositórios no GitHub</h1>
-    </Container>
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/57672887?s=460&u=e68a7e5de1b65ee2657be5b7c64e7d9ad6a8a82c&v=4"
+            alt="Gustavo Harff"
+          />
+          <div>
+            <strong>gustavoharff/my-note-app</strong>
+            <p>Front-end da aplicação My Note App </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
   );
 };
 
