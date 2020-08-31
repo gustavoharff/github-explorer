e42748f650e0984a26a6178739e8a2a396392f30
Gustavo Harff <gustavo.harff@gmail.com>
2020-08-31 10:11:00 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
 
 const App: React.FC = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
 };
 
 export default App;
